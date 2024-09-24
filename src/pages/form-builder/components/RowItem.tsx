82a3f6d1a1b01d627b076e6b27c28e7eef8d1af3
Darko Iliev <darkoilievsum@gmail.com>
2024-09-24 16:08:27 +0200

    fix: types, RowItem components, styled-components

--- src/pages/form-builder/components/RowItem.tsx
+++ src/pages/form-builder/components/RowItem.tsx
@@ -1,27 +1,118 @@
-import { Button, TextField, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+import { forwardRef, useState } from 'react';
+import {
+  Button,
+  TextField,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Radio,
+  RadioGroup,
+  Checkbox,
+} from '@mui/material';
+import { Unstable_NumberInput as BaseNumberInput, NumberInputProps } from '@mui/base/Unstable_NumberInput';
 import { RowItemType } from '../types';
+import { StyledButton, StyledInputElement, StyledInputRoot } from './styled-components';
+import { DraggableProvidedDragHandleProps } from '@hello-pangea/dnd';
 
 interface RowItemProps {
   item: RowItemType;
-  handleProps: any;
+  handleProps: DraggableProvidedDragHandleProps | null;
 }
 
 export const RowItem = ({ item, handleProps }: RowItemProps) => {
+  const [name, setName] = useState('Text Field');
+  const [selectedValue, setSelectedValue] = useState('a');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedValue(event.target.value);
+  };
+
+  const NumberInput = forwardRef(function CustomNumberInput(
+    props: NumberInputProps,
+    ref: React.ForwardedRef<HTMLDivElement>,
+  ) {
+    return (
+      <BaseNumberInput
+        slots={{
+          root: StyledInputRoot,
+          input: StyledInputElement,
+          incrementButton: StyledButton,
+          decrementButton: StyledButton,
+        }}
+        slotProps={{
+          incrementButton: {
+            children: '▴',
+          },
+          decrementButton: {
+            children: '▾',
+          },
+        }}
+        {...props}
+        ref={ref}
+      />
+    );
+  });
+
+  const [value, setValue] = useState<number | null>(null);
+
   const renderItem = (item: RowItemType) => {
     switch (item.content) {
       case 'Text Field':
-        return <TextField label='Text Field' type='text' fullWidth />;
-      case 'Email':
-        return <TextField label='Email' type='email' fullWidth />;
-      case 'File':
-        return <TextField label='File' type='file' fullWidth />;
-      case 'Radio':
-        return <TextField label='Radio' type='radio' fullWidth />;
-      case 'Select':
         return (
           <FormControl>
+            <TextField
+              type='text'
+              id='text-field'
+              value={name}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setName(event.target.value);
+              }}
+            />
+          </FormControl>
+        );
+      case 'Email':
+        return (
+          <FormControl>
+            <TextField label='Email' id='email' fullWidth />
+          </FormControl>
+        );
+      case 'File':
+        return (
+          <FormControl fullWidth>
+            <TextField
+              id='file'
+              type='file'
+              label='File'
+              InputLabelProps={{ shrink: true }}
+              InputProps={{ sx: { display: 'flex', alignItems: 'center' } }}
+              sx={{
+                '& input[type="file"]': {
+                  display: 'flex',
+                  justifyContent: 'flex-start',
+                  alignItems: 'center',
+                  '&::file-selector-button': {
+                    marginRight: '8px',
+                  },
+                },
+              }}
+            />
+          </FormControl>
+        );
+      case 'Radio':
+        return (
+          <FormControl>
+            <RadioGroup row id='radio-group'>
+              <Radio checked={selectedValue === 'a'} onChange={handleChange} value='a' name='radio-buttons' />
+              <Radio checked={selectedValue === 'b'} onChange={handleChange} value='b' name='radio-buttons' />
+            </RadioGroup>
+          </FormControl>
+        );
+      case 'Select':
+        return (
+          <FormControl sx={{ display: 'flex' }}>
             <InputLabel id='select-label'>Select</InputLabel>
-            <Select labelId='select-label' id='select' value={'1'} label='Select' variant={'outlined'}>
+            <Select labelId='select-label' id='select' value={value} label='Select' variant={'outlined'}>
               <MenuItem value={'1'}>Option one</MenuItem>
               <MenuItem value={'2'}>Option two</MenuItem>
               <MenuItem value={'3'}>Option three</MenuItem>
@@ -30,14 +121,20 @@ export const RowItem = ({ item, handleProps }: RowItemProps) => {
           </FormControl>
         );
       case 'Checkbox':
-        return <TextField label='Checkbox' type='checkbox' fullWidth />;
+        return (
+          <FormControl>
+            <Checkbox inputProps={{ 'aria-label': 'checkbox' }} id='checkbox' />
+          </FormControl>
+        );
       case 'Button':
         return (
           <Button
-            variant='outlined'
             fullWidth
+            variant='outlined'
+            id='button'
             {...handleProps}
             onClick={(e) => {
+              console.log('handleProps', handleProps);
               e.preventDefault();
               e.stopPropagation();
             }}
@@ -46,9 +143,21 @@ export const RowItem = ({ item, handleProps }: RowItemProps) => {
           </Button>
         );
       case 'Number':
-        return <TextField label='Number' type='number' fullWidth />;
+        return (
+          <NumberInput
+            aria-label='number-input'
+            placeholder='Type a number…'
+            id='number-input'
+            value={value}
+            onChange={(_event, val) => setValue(val)}
+          />
+        );
       case 'Textarea':
-        return <TextField label='Textarea' type='text' multiline fullWidth />;
+        return (
+          <FormControl sx={{ display: 'flex' }}>
+            <TextField label='Textarea' type='text' id='text-area' multiline fullWidth />
+          </FormControl>
+        );
       default:
         return null;
     }
--- src/pages/form-builder/components/styled-components.ts
+++ src/pages/form-builder/components/styled-components.ts
@@ -1,4 +1,6 @@
 import { styled } from '@mui/material/styles';
+import { numberInputClasses } from '@mui/base/Unstable_NumberInput';
+import { grey } from '@mui/material/colors';
 
 export const Content = styled('div')(() => ({
   marginRight: '200px',
@@ -117,3 +119,111 @@ export const IconButton = styled('div')(() => ({
     color: 'lightgray',
   },
 }));
+
+export const StyledInputRoot = styled('div')(
+  ({ theme }) => `
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-weight: 400;
+  border-radius: 8px;
+  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+  border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
+  box-shadow: 0px 2px 2px ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
+  display: grid;
+  grid-template-columns: 1fr 19px;
+  grid-template-rows: 1fr 1fr;
+  overflow: hidden;
+  column-gap: 8px;
+  padding: 4px;
+
+  &.${numberInputClasses.focused} {
+    border-color: ${grey[400]};
+    box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? grey[600] : grey[200]};
+  }
+
+  &:hover {
+    border-color: ${grey[400]};
+  }`,
+);
+
+export const StyledInputElement = styled('input')(
+  ({ theme }) => `
+  font-size: 0.875rem;
+  font-family: inherit;
+  font-weight: 400;
+  line-height: 1.5;
+  grid-column: 1/2;
+  grid-row: 1/3;
+  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  background: inherit;
+  border: none;
+  border-radius: inherit;
+  padding: 8px 12px;
+  outline: 0;`,
+);
+
+export const StyledButton = styled('button')(
+  ({ theme }) => `
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+  appearance: none;
+  padding: 0;
+  width: 19px;
+  height: 19px;
+  font-family: system-ui, sans-serif;
+  font-size: 0.875rem;
+  line-height: 1;
+  box-sizing: border-box;
+  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+  border: 0;
+  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  transition-property: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 120ms;
+
+  &:hover {
+    background: ${theme.palette.mode === 'dark' ? grey[800] : grey[50]};
+    border-color: ${theme.palette.mode === 'dark' ? grey[600] : grey[300]};
+    cursor: pointer;
+  }
+
+  &.${numberInputClasses.incrementButton} {
+    grid-column: 2/3;
+    grid-row: 1/2;
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+    border: 1px solid;
+    border-bottom: 0;
+    &:hover {
+      cursor: pointer;
+      background: ${grey[400]};
+      color: ${grey[50]};
+    }
+
+  border-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[200]};
+  background: ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
+  color: ${theme.palette.mode === 'dark' ? grey[200] : grey[900]};
+  }
+
+  &.${numberInputClasses.decrementButton} {
+    grid-column: 2/3;
+    grid-row: 2/3;
+    border-bottom-left-radius: 4px;
+    border-bottom-right-radius: 4px;
+    border: 1px solid;
+    &:hover {
+      cursor: pointer;
+      background: ${grey[400]};
+      color: ${grey[50]};
+    }
+
+  border-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[200]};
+  background: ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
+  color: ${theme.palette.mode === 'dark' ? grey[200] : grey[900]};
+  }
+  & .arrow {
+    transform: translateY(-1px);
+  }`,
+);
