312754e1c09b013d9976edb98f144a7f2bd3475e
Darko Iliev <darkoilievsum@gmail.com>
2024-11-08 19:30:00 +0100

    fix: Fix bug in ITEMS object, add index

--- src/pages/forms/constants.ts
+++ src/pages/forms/constants.ts
@@ -2,38 +2,47 @@ export const ITEMS = {
   'item-1': {
     id: 'item-1',
     content: 'Text Field',
+    index: 0,
   },
   'item-2': {
     id: 'item-2',
     content: 'Email',
+    index: 1,
   },
   'item-3': {
     id: 'item-3',
     content: 'File',
+    index: 2,
   },
   'item-4': {
     id: 'item-4',
     content: 'Radio',
+    index: 3,
   },
   'item-5': {
     id: 'item-5',
     content: 'Select',
+    index: 4,
   },
   'item-6': {
     id: 'item-6',
     content: 'Checkbox',
+    index: 5,
   },
   'item-7': {
     id: 'item-7',
     content: 'Button',
+    index: 6,
   },
   'item-8': {
     id: 'item-8',
     content: 'Number',
+    index: 7,
   },
   'item-9': {
     id: 'item-9',
     content: 'Textarea',
+    index: 8,
   },
 };
 
